54278c592dad3ebec3fc9a9d9bf887f964bd36b9
Cherhazad <cherhazad.boustani@outlook.fr>
2024-05-29 21:26:30 +0200

    TP nettoyage - Exerccie 4

---
+++ src/main/java/ex4/TestDataUtils.java
@@ -0,0 +1,16 @@
+package ex4;
+
+import java.util.Date;
+
+public class TestDataUtils {
+
+	public static void main(String[] args) {
+		
+		Date today = new Date();
+		String essai = DateUtils.format(today);
+		
+		System.out.println(essai);
+
+	}
+
+}
